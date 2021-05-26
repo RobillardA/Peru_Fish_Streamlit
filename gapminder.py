c81af60c666a1bf53f47614f86c0c7671455395a
Mike Trizna <triznam@si.edu>
2021-05-26 11:50:44 -0400

    Added continent filter to gapminder demo

--- gapminder.py
+++ gapminder.py
@@ -13,15 +13,20 @@ def load_data():
     data = pd.read_csv(DATA_URL, sep='\t')
     return data
 
-def bubble_chart(df, year):
-    df_year = df[df['year'] == year].copy()
+def bubble_chart(df, year, continent = 'All'):
+    if continent == 'All':
+        df_year = df[df['year'] == year].copy()
+        chart_title = f'{year}: All continents'
+    else:
+        df_year = df[(df['year'] == year) & (df['continent'] == continent)]
+        chart_title = f'{year}: {continent}'
     alt_chart = alt.Chart(df_year).mark_circle().encode(
         alt.X('gdpPercap:Q', scale=alt.Scale(type='log', domain=(100, 100000))),
         alt.Y('lifeExp:Q', scale=alt.Scale(domain=(25, 90))),
         size=alt.Size('pop:Q'),
         color='continent:N',
         tooltip=['country', 'lifeExp', 'pop', 'gdpPercap']
-    ).properties(title=str(year)).interactive()
+    ).properties(title=chart_title).interactive()
     return alt_chart
 
 data_load_state = st.text('Loading data...')
@@ -30,7 +35,10 @@ data_load_state.text("Done!")
 
 possible_years = data['year'].unique()
 
+continent = st.selectbox('Cotinent',
+                        ['All','Asia', 'Europe', 'Africa', 'Americas', 'Oceania'])
+
 gap_year = st.slider('Year', min_value=1952, max_value=2007, step=5)
 
-test_chart = bubble_chart(data, gap_year)
+test_chart = bubble_chart(data, gap_year, continent)
 st.altair_chart(test_chart, use_container_width=True)
